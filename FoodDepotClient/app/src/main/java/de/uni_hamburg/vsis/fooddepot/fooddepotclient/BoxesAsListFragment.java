4870234bc2ba9d482c1516f515500f0962b17064
philly-vanilly <2lysenko@informatik.uni-hamburg.de>
2016-06-16 10:39:07 +0200

    list fragment implemented but not tested yet

--- FoodDepotClient/app/src/main/java/de/uni_hamburg/vsis/fooddepot/fooddepotclient/BoxesAsListFragment.java
+++ FoodDepotClient/app/src/main/java/de/uni_hamburg/vsis/fooddepot/fooddepotclient/BoxesAsListFragment.java
@@ -1,25 +1,17 @@
 package de.uni_hamburg.vsis.fooddepot.fooddepotclient;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import rest.beans.Box;
@@ -27,18 +19,66 @@ import rest.beans.Box;
 public class BoxesAsListFragment extends Fragment implements BoxesFragmentInterface {
 
     private final String TAG = "BoxesAsListFragment";
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private RecyclerView mBoxesListRecyclerView;
+    private BoxesListAdapter mBoxesListAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        // RecyclerView recycles views and positions them on the screen by using a
+        // LayoutManager. LayoutManager defines positioning and Scrolling behavior.
+        mBoxesListRecyclerView = (RecyclerView) view.findViewById(R.id.boxes_list_recycler_view);
+        mBoxesListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //List<Box> boxes = getBoxes();
+        List<Box> boxes = new ArrayList<Box>(20); //TODO: replace placeholder
+
+        mBoxesListAdapter = new BoxesListAdapter(boxes);
+        mBoxesListRecyclerView.setAdapter(mBoxesListAdapter);
+
+        return view;
+    }
+
+    // class is managed by RecyclerView
+    private class BoxesHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewBoxesName;
+
+        public BoxesHolder(View itemView) {
+            super(itemView);
+            mTextViewBoxesName = (TextView) itemView;
+        }
+    }
+
+    // RecyclerView will communicate with this adapter when ViewHolder needs to be created or
+    // connected with a Note object
+    private class BoxesListAdapter extends RecyclerView.Adapter<BoxesHolder>{
+
+        private List<Box> mBoxes;
+
+        public BoxesListAdapter(List<Box> boxes){
+            mBoxes = boxes;
+        }
+
+        @Override
+        public BoxesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            BoxesHolder boxesHolder = new BoxesHolder(view);
+            return boxesHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(BoxesHolder holder, int position) {
+            Box box = mBoxes.get(position);
+            holder.mTextViewBoxesName.setText(box.getName());
+        }
+        
+        @Override
+        public int getItemCount() {
+            return mBoxes.size();
+        }
     }
 
     @Override
--- FoodDepotClient/app/src/main/java/rest/beans/Box.java
+++ FoodDepotClient/app/src/main/java/rest/beans/Box.java
@@ -7,8 +7,37 @@ public class Box {
     public String id;
     public String name;
     public String content;
-
     public double latitude;
     public double longitude;
 
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
