2787d687a0259726c5b1245c0c732f77b36ab696
Philip L <2lysenko@informatik.uni-hamburg.de>
2016-06-16 19:03:55 +0200

    new interface for pulling box data

---
+++ FoodDepotClient/app/src/main/java/de/uni_hamburg/vsis/fooddepot/fooddepotclient/BoxActivityInterface.java
@@ -0,0 +1,11 @@
+package de.uni_hamburg.vsis.fooddepot.fooddepotclient;
+
+/**
+ * Created by sysadmin on 16/06/16.
+ */
+public interface BoxActivityInterface {
+
+    public void requestBoxListUpdate();
+
+
+}
